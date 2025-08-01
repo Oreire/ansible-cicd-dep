e7a60ae8ffae58376e008e998a8ae7bdccb16ee8
LAREDO LANCER <lancerajayi@yahoo.com>
2025-08-01 23:51:36 +0100

    Test Deployment

--- TERRA/main.tf
+++ TERRA/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
+
 resource "null_resource" "prepare_ansible_dir" {
   provisioner "local-exec" {
     command = "mkdir -p ../ansible"
@@ -9,28 +10,43 @@ resource "null_resource" "prepare_ansible_dir" {
 }
 
 resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "DevOpsEC2"
-  }
-
-  key_name      = var.key_name
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
   associate_public_ip_address = true
-
+  
   provisioner "local-exec" {
     command = "echo ${self.public_ip} > ../ansible/hosts.txt"
   }
-}
-# resource "aws_security_group" "web_sg" {
-#   name        = "web_sg"
-#   description = "Allow HTTP and SSH traffic"
 
-#     ingress {
-#       from_port   = 22
-#       to_port     = 22
-#       protocol    = "tcp"
-#       cidr_blocks = ["YOUR_TRUSTED_IP/32"]
-#     }
-#   }
+  depends_on = [null_resource.prepare_ansible_dir]
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP and SSH traffic"
+  #vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR_TRUSTED_IP/32"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.1.0/24"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #tags = var.tags
+}
--- TERRA/variable.tf
+++ TERRA/variable.tf
@@ -1,25 +1,24 @@
-
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
   default     = "eu-west-2"
-  
 }
 
 variable "ami_id" {
-  description = "The AMI ID to use for the instance"
+  description = "The AMI ID to use for the EC2 instance"
   type        = string
-  default     = "ami-044415bb13eee2391"
+  default     = "ami-044415bb13eee2391" # Use the most accurate or latest AMI here
 }
 
 variable "instance_type" {
-  description = "The instance type to use for the instance"
+  description = "The EC2 instance type to deploy"
   type        = string
   default     = "t2.micro"
 }
 
 variable "key_name" {
-  description = "The name of the key pair to use for SSH access"
+  description = "SSH key pair name used for EC2 access"
   type        = string
-  default     = "maven_key"
-}       
+  default     = "maven_key" # Consistency with your Ansible key config
+}
+
