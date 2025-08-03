c578ac1c0d7f470251905ff0b74f1cdd27d4246e
LAREDO LANCER <lancerajayi@yahoo.com>
2025-08-03 01:12:07 +0100

    Testing main.tf

--- TERRA/main.tf
+++ TERRA/main.tf
@@ -42,6 +42,7 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = { Name = "main-igw" }
 }
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -51,14 +52,16 @@ resource "aws_route_table" "public" {
   }
 }
 
-
-resource "aws_subnet" "public" {
+resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-west-2a"
-  tags = { Name = "public-subnet" }
+  tags = {
+    Name = "main-public-subnet"
+  }
 }
+
 
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public.id
