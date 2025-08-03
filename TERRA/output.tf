01f9d60bdc98dbbd15c165f8e91fdd42cc1eec23
LAREDO LANCER <lancerajayi@yahoo.com>
2025-08-03 01:23:04 +0100

    Refactoring

--- TERRA/output.tf
+++ TERRA/output.tf
@@ -11,3 +11,14 @@ output "security_group_id" {
   description = "ID of the security group for the EC2 instance"
   value       = aws_security_group.ansible_sg2.id
 }
+output "subnet_vpc" {
+  value = aws_subnet.public.vpc_id
+}
+
+output "sg_vpc" {
+  value = aws_security_group.ansible_sg2.vpc_id
+}
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
